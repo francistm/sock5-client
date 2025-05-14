872a92f9b782f2e896c90aff7d40da4e5ba5d7c0
francis <francis.tm@gmail.com>
2025-05-14 18:11:56 +0800

    move data_recv, data_send to stack

--- client.c
+++ client.c
@@ -69,7 +69,9 @@ void sock5_client_free(SOCK5Client *client)
 
 int sock5_client_connect(SOCK5Client *client, const char *dst_host, int dst_port, SOCK5Auth *auth)
 {
-    unsigned char phase = 0;
+    char phase          = 0;
+    char data_send[513] = { 0 };
+    char data_recv[262] = { 0 };
 
     while (1)
     {
@@ -77,9 +79,6 @@ int sock5_client_connect(SOCK5Client *client, const char *dst_host, int dst_port
         {
             case 0: // greeting
             {
-                char data_send[4] = { 0 };
-                char data_recv[2] = { 0 };
-
                 data_send[0] = 0x05;                          // version
                 data_send[1] = 2;                             // auth method size
                 data_send[2] = AUTH_METHOD_NONE;              // method 1
@@ -100,13 +99,19 @@ int sock5_client_connect(SOCK5Client *client, const char *dst_host, int dst_port
                     return 0;
                 }
 
-                if (data_recv[1] == AUTH_METHOD_NONE) phase = 2;
-                else if (data_recv[1] == AUTH_METHOD_USERNAME_PASSWORD) phase = 1;
-                else client->err_code = SOCK5_CLIENT_ERR_GREETING_FAIL;
+                if (data_recv[1] == AUTH_METHOD_NONE) {
+                    phase = 2;
+                    break;
+                }
 
-                if (client->err_code != SOCK5_CLIENT_ERR_OK) return 0;
+                if (data_recv[1] == AUTH_METHOD_USERNAME_PASSWORD)
+                {
+                    phase = 1;
+                    break;
+                }
 
-                break;
+                client->err_code = SOCK5_CLIENT_ERR_GREETING_FAIL;
+                return 0;
             }
 
             case 1: // auth
@@ -116,35 +121,29 @@ int sock5_client_connect(SOCK5Client *client, const char *dst_host, int dst_port
                     return 0;
                 }
 
-                size_t idlen = strlen(auth->username);
-                size_t pwlen = strlen(auth->password);
-                char *data_send   = (char *)malloc(3 + idlen + pwlen);
-                char data_recv[2] = { 0 };
+                char id_len = (char)strlen(auth->username);
+                char pw_len = (char)strlen(auth->password);
 
-                data_send[0]         = 0x05;
-                data_send[1]         = idlen;
-                data_send[2 + idlen] = pwlen;
-                memcpy(data_send + 2        , auth->username, idlen);
-                memcpy(data_send + 3 + idlen, auth->password, pwlen);
+                data_send[0]          = 0x05;
+                data_send[1]          = id_len;
+                data_send[2 + id_len] = pw_len;
+                memcpy(data_send + 2,          auth->username, id_len);
+                memcpy(data_send + 3 + id_len, auth->password, pw_len);
 
-                if (send(client->fd, data_send, 3 + idlen + pwlen, 0) < 0) {
+                if (send(client->fd, data_send, 3 + id_len + pw_len, 0) < 0) {
                     client->err_code = SOCK5_CLIENT_ERR_AUTH_FAIL;
-                    goto client_connect_auth_cleanup;
+                    return 0;
                 }
 
                 if (recv(client->fd, data_recv, 2, 0) < 0) {
                     client->err_code = SOCK5_CLIENT_ERR_AUTH_FAIL;
-                    goto client_connect_auth_cleanup;
+                    return 0;
                 }
 
                 if (data_recv[0] != 0x05 || data_recv[1] != 0x00) {
                     client->err_code = SOCK5_CLIENT_ERR_AUTH_FAIL;
-                    goto client_connect_auth_cleanup;
+                    return 0;
                 }
-
-                client_connect_auth_cleanup:
-                free(data_send);
-                if (client->err_code != SOCK5_CLIENT_ERR_OK) return 0;
 
                 phase = 2;
                 break;
@@ -153,65 +152,54 @@ int sock5_client_connect(SOCK5Client *client, const char *dst_host, int dst_port
             case 2: // connect
             {
                 size_t next_read_sz = 0;
-                char host_len       = strlen(dst_host);
-                char *data_send     = (char *)malloc(7 + host_len);
-                char *data_recv     = (char *)malloc(5);           // realloc later
+                char   dst_host_len = (char)strlen(dst_host);
 
-                data_send[0]            = 0x05;                    // version
-                data_send[1]            = COMMAND_CONNECT;         // command
-                data_send[2]            = 0x00;                    // reserved
-                data_send[3]            = ADDRESS_TYPE_DOMAIN;     // address type
-                data_send[4]            = host_len;                // address length
-                data_send[5 + host_len] = (dst_port >> 8) & 0xFF;  // port high byte
-                data_send[6 + host_len] = (dst_port)      & 0xFF;  // port low byte
-                memcpy(data_send + 5, dst_host, host_len);         // address
+                data_send[0]                = 0x05;                   // version
+                data_send[1]                = COMMAND_CONNECT;        // command
+                data_send[2]                = 0x00;                   // reserved
+                data_send[3]                = ADDRESS_TYPE_DOMAIN;    // address type
+                data_send[4]                = dst_host_len;           // address length
+                data_send[5 + dst_host_len] = (dst_port >> 8) & 0xFF; // port h
+                data_send[6 + dst_host_len] = (dst_port)      & 0xFF; // port l
+                memcpy(data_send + 5, dst_host, dst_host_len);        // address
 
-                if (send(client->fd, data_send, 7 + host_len, 0) < 0) {
+                if (send(client->fd, data_send, 7 + dst_host_len, 0) < 0) {
                     client->err_code = SOCK5_CLIENT_ERR_CONNECT_DST_FAIL;
-                    goto client_connect_on_connect_cleanup;
+                    return 0;
                 }
 
                 if (recv(client->fd, data_recv, 5, 0) < 0) {
                     client->err_code = SOCK5_CLIENT_ERR_CONNECT_DST_FAIL;
-                    goto client_connect_on_connect_cleanup;
+                    return 0;
                 }
 
                 if (data_recv[0] != 0x05) {
                     client->err_code = SOCK5_CLIENT_ERR_CONNECT_DST_FAIL;
-                    goto client_connect_on_connect_cleanup;
+                    return 0;
                 }
 
                 if (data_recv[1] != CONN_REPLY_SUCCEEDED) {
                     client->err_code = SOCK5_CLIENT_ERR_CONNECT_DST_FAIL;
-                    goto client_connect_on_connect_cleanup;
+                    return 0;
                 }
 
                 if (data_recv[3] == ADDRESS_TYPE_IPV4) {
                     next_read_sz = 5;
-                    data_recv    = (char *)realloc(data_recv, 10);
                 } else if (data_recv[3] == ADDRESS_TYPE_DOMAIN) {
                     next_read_sz = 2 + data_recv[4];
-                    data_recv    = (char *)realloc(data_recv, 7 + data_recv[4]);
                 }
 
                 if (next_read_sz == 0) {
                     client->err_code = SOCK5_CLIENT_ERR_CONNECT_DST_FAIL;
-                    goto client_connect_on_connect_cleanup;
+                    return 0;
                 }
 
                 if (recv(client->fd, data_recv + 5, next_read_sz, 0) < 0) {
                     client->err_code = SOCK5_CLIENT_ERR_CONNECT_DST_FAIL;
-                    goto client_connect_on_connect_cleanup;
+                    return 0;
                 }
 
                 client->err_code = SOCK5_CLIENT_ERR_OK;
-
-                client_connect_on_connect_cleanup:
-                free(data_send);
-                free(data_recv);
-
-                if (client->err_code != SOCK5_CLIENT_ERR_OK) return 0;
-
                 return 1;
             }
         }
